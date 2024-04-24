268942e78c86ec8acbec68c69cc780e57f1802b2
ZaraEnti <zara.zhang@enti.cat>
2024-04-24 11:53:01 +0200

    ejercicio incompleto

--- characters_bs4.py
+++ characters_bs4.py
@@ -28,3 +28,36 @@ for character in characters:
     level = character.level['value']
 
     print(f"\t{name} \t{age} \t{gender} \t{level}")
+
+encontrado = False
+while not encontrado:
+    id=input ("Intrioduce número: ")
+    file = open('characters.facx', 'r')
+    soup = BeautifulSoup(file, 'xml')
+    file.close()
+    character = soup.find('character', {'id':id})
+    if not character:
+        print("Error")
+    else:
+        encontrado = True
+
+
+print("Personaje escojido es:")
+print(f"\tNombre: {character.find('name').text}")
+print("Personaje escojid")
+print("Personaje escojido es :")
+print("Personaje escojido es :")
+
+#mostrar los items del personaje eleejido. Buscar el character
+with open ('chara_items.facix', 'r') as file:
+    soup = BeautifulSoup(file, 'xml')
+
+
+charaItems = soup.find_all('charaItems')
+
+for charaItem in charaItems:
+    id_items = charaItem.find('items', {'id', id})
+
+print(id_items)
+                 
+             
