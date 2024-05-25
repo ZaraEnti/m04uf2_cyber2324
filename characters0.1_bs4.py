b8f1999d66dba467ab744bcdd0979502e04634f9
zara <zara.zhang@enti.cat>
2024-05-25 23:23:42 +0200

    entrega

--- characters0.1_bs4.py
+++ characters0.1_bs4.py
@@ -2,60 +2,65 @@
 
 from bs4 import BeautifulSoup
 file = open('characters.facx', 'r')#ponemos el modo de lectura
-soup = BeautifulSoup(file, 'xml')
-characters = soup.find_all ("character")
-file.close()
+soup = BeautifulSoup(file, 'xml')#tipo de documento
 
-title = "Bienbenido/a al Faryadventures"
+characters = soup.find_all ("character")
+file.close()#una vez sacamos la información pues lo podemos cerrar ya que esta guardada en una variable
+
+title = "\nBienbenido/a al Faryadventures"
 
 print(title)
-print("="*len(title))
-
 
 for character in characters:
     print(f"{character['id']}\t{character.find('name').text}")
- 
-num = input("\nElija su personaje:\n")
 
-print("")
+option = input("Quieres matar a un  personaje?(y/n)")
+if option == 'y':
+    num = input("\nElija a quien quiere matar: ")
+    
+    kill_character = soup.find('character', {'id' : num})#personaje elegido para elminar
+    
 
-file = open('characters.facx', 'r')
-soup = BeautifulSoup(file, 'xml')
-file.close()
-
-character = soup.find('character', {'id' : num})#buscar a partir del id el parent
-
-print("Elpersonaje elejido es:\n")
-print(f"\tNombre: {character.find('name').text}")
-print(f"\tEdad: {character.find('age').text}")
-print(f"\tGénero: {character.find('gender')['value']}")
-print(f"\tNivel: {character.find('level')['value']}")
+    #bucle para tener nuetra nueva cadena de caracteres
+    for character in characters:
+        if kill_character == character:
+            characters.remove(kill_character)#función remove para quitar el personaje
+            print(f"Has matado: {character.find('name').text}")
+else:        
 
 
-#Sacar el nombre a partir de la tabla intermedia
-file = open ('chara_items.facix', 'r')
-soup = BeautifulSoup(file, 'xml')
-file.close()
+    num = input("\nElija su personaje:\n")
 
-charaItem = soup.find('character', {'id': num}).parent
-id_item = charaItem.find('items')
+    print("")
 
-file = open('characters.faix', 'r')
-soup = BeautifulSoup(file,'xml')
-file.close()
+    character = soup.find('character', {'id' : num})#buscar a partir del id el parent
 
-item= soup.find('item', {'id': id_item['id']})
-name_item = item.find('item').text
+    print("El personaje elejido es:\n")
+    print(f"\tNombre: {character.find('name').text}")
+    print(f"\tEdad: {character.find('age').text}")
+    print(f"\tGénero: {character.find('gender')['value']}")
+    print(f"\tNivel: {character.find('level')['value']}")
 
-print(f"\tItem: {name_item}")
 
-file = open ('chara_items.facix', 'r')
-soup = BeautifulSoup(file, 'xml')
-file.close()
-characters_items = soup.find_all('character_item')
+    #Sacar el nombre a partir de la tabla intermedia
+    file = open ('characters_items.facix', 'r')
+    soup = BeautifulSoup(file, 'xml')
+    file.close()
 
-for character_item in characters_itmes:
-    id_character = character_item.find("character")['id']
+    charaItem = soup.find('character', {'id': num}).parent#da las etiquetas de la relación
+    #print(charaItem)
 
-    if id_character == id:
-        id_item == character_item.find
+    id_items = charaItem.find_all('items')#todos los items que tiene esa id relación
+    #print(id_items)
+
+    #la tabla de items
+    file = open('characters.faix', 'r')
+    soup = BeautifulSoup(file,'xml')
+    file.close()
+
+    #me aseguro que nos imprime todos los items que tiene este personaje
+    for id_item in id_items:
+        item = soup.find('item', {'id': id_item['id']})
+        name_item = item.find('item').text
+        print(f"\tItem: {name_item}")
+
